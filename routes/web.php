02a754c1d801ac257dfddf86c704b1355bb0bbbd
Bijay <bijaydhakla115@gmail.com>
2026-09-09 17:30:48 +0545

    feat: add about page route

--- routes/web.php
+++ routes/web.php
@@ -4,3 +4,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::inertia('/home', 'Home')->name('home');
 Route::inertia('/destinations', 'Destination')->name('destinations');
+Route::inertia('/about', 'About')->name('about');
