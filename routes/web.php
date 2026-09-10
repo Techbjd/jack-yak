2416338f00618958912d7b886c885e51b0cd70bb
Bijay <bijaydhakla115@gmail.com>
2026-09-10 14:14:47 +0545

    feat(routes): restore about route

--- routes/web.php
+++ routes/web.php
@@ -6,5 +6,6 @@ Route::inertia('/home', 'Home')->name('home');
 Route::inertia('/destinations', 'Destination')->name('destinations');
 Route::inertia('/view-all', 'ViewAll')->name('view-all');
 Route::inertia('/guide', 'Guide')->name('guide');
+Route::inertia('/about', 'About')->name('about');
 Route::inertia('/login', 'Login')->name('login');
 Route::inertia('/form', 'Quiz')->name('form');
