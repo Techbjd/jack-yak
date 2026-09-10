f60b8563803a033f4772e6dbfe560aa53bb9f9e0
Bijay <bijaydhakla115@gmail.com>
2026-09-10 14:26:49 +0545

    feat(routes): add user route

--- routes/web.php
+++ routes/web.php
@@ -7,5 +7,6 @@ Route::inertia('/destinations', 'Destination')->name('destinations');
 Route::inertia('/view-all', 'ViewAll')->name('view-all');
 Route::inertia('/guide', 'Guide')->name('guide');
 Route::inertia('/about', 'About')->name('about');
+Route::inertia('/user', 'User')->name('user');
 Route::inertia('/login', 'Login')->name('login');
 Route::inertia('/form', 'Quiz')->name('form');
